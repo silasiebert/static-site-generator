a978d1abc11fc4308d4cd65f1f409da22720335e
silasiebert <sila.siebert@gmail.com>
2024-05-20 18:12:16 -0300

    Ch 5: Website 1. Copy Static - Recursive function to copy files from one directory to another

--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
-from textnode import TextNode 
+from os.path import isfile
+from textnode import TextNode
+import os
+import shutil
+
 
 def main():
     dummy = TextNode("This is a textnode", "bold", "www.boot.dev")
     print(dummy)
+    copy_directory("./static", "./public")
+
+
+def copy_directory(source_directory, target_directory):
+    shutil.rmtree(target_directory)
+    os.mkdir(target_directory)
+    # Validate paths
+    if not os.path.exists(source_directory):
+        raise Exception("Invalid source directory")
+    if not os.path.exists(target_directory):
+        raise Exception("Invalid target directory")
+    # List files in source dir
+    source_files = os.listdir(source_directory)
+    for entry in source_files:
+        current_path = os.path.join(source_directory, entry)
+        target_path = os.path.join(target_directory, entry)
+        print(f"Current directory: {current_path}")
+        if os.path.isfile(current_path):
+            # copy to target dir
+            print(f"I'm a file: {current_path}")
+            shutil.copy(current_path, target_path)
+        else:
+            os.mkdir(target_path)
+            copy_directory(current_path, target_path)
+
 
 main()
